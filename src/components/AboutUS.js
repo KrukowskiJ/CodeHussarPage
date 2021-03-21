94d112729b70a3217058689eb83391ae3897160d
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-03-21 01:31:57 +0100

    fix articles carusel

--- src/components/AboutUS.js
+++ src/components/AboutUS.js
@@ -4,8 +4,10 @@ import Slider from "react-slick";
 import Pulse from '../images/articles/wat.png'
 import ReadyDonate from '../images/articles/ic 21.png'
 import Uninetwork from '../images/articles/kokon.png'
-import ProjectCard from '../components/micros/ProjectCard.js'
+import ProjectCard from '../components/micros/ArticleCard.js'
 import "./slick.css";
+import PrevArrowImg from "../images/prevarrow.png"
+import NextArrowImg from "../images/nextarrow.png"
 
 var projectNameList = ["Pulse", "Uninetwork", "ReadyDonate"];
 var projectPictureList = [Pulse, Uninetwork, ReadyDonate];
@@ -23,33 +25,55 @@ const AboutUS = () => {
     const [projectPicture, setProjectPicture] = useState(projectPictureList[0]);
 
 
+    function PrevArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                onClick={onClick}
+            >
+                <ArrowBox src={PrevArrowImg} />
+            </div>
+        );
+    }
 
+    function NextArrow(props) {
+        const { className, style, onClick } = props;
+        return (
+            <div
+                className={className}
+                onClick={onClick}
+            >
+                <ArrowBox src={NextArrowImg} />
+            </div>
+        );
+    }
 
     const settings = {
         centerPadding: 0,
         centerMode: true,
-        cssEase: "linear",
-        autoplay: false,
+        cssEase: 'cubic-bezier(0,.67,1,.34)',
+        autoplay: true,
         speed: 2000,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 4000,
         infinite: true,
         dots: true,
-        slidesToShow: 3,
+        slidesToShow: 7,
         slidesToScroll: 1,
-        infinite: true,
-        dots: true,
+        prevArrow: <PrevArrow />,
+        nextArrow: <NextArrow />,
         responsive: [
             {
-                breakpoint: 1024,
+                breakpoint: 1124,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 5,
                     slidesToScroll: 1,
                 }
             },
             {
-                breakpoint: 800,
+                breakpoint: 900,
                 settings: {
-                    slidesToShow: 1,
+                    slidesToShow: 3,
                     slidesToScroll: 1,
                 }
             }
@@ -63,12 +87,11 @@ const AboutUS = () => {
         window.open(projectLinkList[prjNumber - 1], '_blank');
     }
     const setProject = (prjNumber) => {
-        console.log(projectPictureList[prjNumber - 1]);
         return (<>
-
-            <ProjectCard img={projectPictureList[prjNumber - 1]} />
-            <ProjectName style={{ marginTop: "30px" }}>{projectNameList[prjNumber - 1]}</ProjectName>
-        </>
+            <CARD>
+                <ProjectCard img={projectPictureList[prjNumber - 1]} />
+                <ProjectName style={{ marginTop: "30px" }}>{projectNameList[prjNumber - 1]}</ProjectName>
+            </CARD></>
         );
     }
 
@@ -79,34 +102,46 @@ const AboutUS = () => {
             About Us
         </HeaderTitle>
 
+        < CaruselBack>
+            <Slider {...settings}>
 
-        <Slider {...settings}>
+                <div onClick={() => { setProjectInfo(1) }}>
+                    {setProject(1)}
+                </div>
 
-            <div onClick={() => { setProjectInfo(1) }}>
-                {setProject(1)}
-            </div>
+                <div onClick={() => { setProjectInfo(2) }}>
+                    {setProject(2)}
+                </div>
 
-            <div onClick={() => { setProjectInfo(2) }}>
-                {setProject(2)}
-            </div>
+                <div onClick={() => { setProjectInfo(3) }}>
+                    {setProject(3)}
+                </div>
+                <div onClick={() => { setProjectInfo(1) }}>
+                    {setProject(1)}
+                </div>
 
-            <div onClick={() => { setProjectInfo(3) }}>
-                {setProject(3)}
-            </div>
-            <div onClick={() => { setProjectInfo(1) }}>
-                {setProject(1)}
-            </div>
+                <div onClick={() => { setProjectInfo(2) }}>
+                    {setProject(2)}
+                </div>
 
-            <div onClick={() => { setProjectInfo(2) }}>
-                {setProject(2)}
-            </div>
+                <div onClick={() => { setProjectInfo(3) }}>
+                    {setProject(3)}
+                </div>
+                <div onClick={() => { setProjectInfo(1) }}>
+                    {setProject(1)}
+                </div>
 
-            <div onClick={() => { setProjectInfo(3) }}>
-                {setProject(3)}
-            </div>
+                <div onClick={() => { setProjectInfo(2) }}>
+                    {setProject(2)}
+                </div>
 
-        </Slider>
+                <div onClick={() => { setProjectInfo(3) }}>
+                    {setProject(3)}
+                </div>
 
+            </Slider>
+            <RedLine></RedLine>
+        </ CaruselBack>
 
     </About>)
 
@@ -114,14 +149,12 @@ const AboutUS = () => {
 
 
 const About = styled.div`
-    margin-top: 30vh;
-    margin-bottom: 30vh;
+    margin-bottom: 10vh;
 `
 
 const HeaderTitle = styled.h1`
     text-align:center;
     color:black;
-    margin: 100px;
     font-size: 3em;
 `
 
@@ -132,10 +165,28 @@ const ProjectName = styled.h1`
     color:black;
     
 `
+const CARD = styled.div`
+justify-content:center;
+align-items: center;
+`
+const CaruselBack = styled.div`
+margin: 80px;
+`
+const ArrowBox = styled.img`
+margin:0px;
 
+:hover{
 
+    transform: scale(1.2);
 
+}
+`
 
-
+const RedLine = styled.div`
+background-color:#A72727;
+    width:100%;
+    height:2px;
+    margin-top: 40px;
+`
 
 export default AboutUS
