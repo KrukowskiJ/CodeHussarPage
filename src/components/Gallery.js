3ed031bc5c3bb4fa3dc40b8cf09a33aa14d969d0
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-03-22 02:45:29 +0100

    gallery grayscale

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -13,16 +13,17 @@ import Image8 from '../images/bhl.jpg';
 import Zoom from 'react-reveal/Zoom';
 
 
-const ImagePhotoFull=styled.img`
+const ImagePhotoFull = styled.img`
     width:100%; 
     height:100%; 
     padding:0.5vw;
     -webkit-transition: 0.3s ease-in-out, -webkit-transform: 0.3s ease-in-out;
     -moz-transition: 0.3s ease-in-out, -moz-transform 0.3s ease-in-out;
     transition: 0.3s ease-in-out, transform 0.3s ease-in-out;
-
+    filter: blur(0px) grayscale(0.1) opacity(0.8);
     &: hover
     {
+        filter: blur(0px) grayscale(0) opacity(1);
         transform:scale(1.03);
     }
 `
@@ -41,67 +42,67 @@ const styles = {
     }
 };
 
-const PhotoContainer=styled.div`
+const PhotoContainer = styled.div`
     margin-right: 5vw;
     margin-left: 5vw;
 `
 
-export default ()=>{
-    return(
+export default () => {
+    return (
         <>
             <Container fluid>
-                    <PhotoContainer>
-                       <Row>
+                <PhotoContainer>
+                    <Row>
                         <Col md="7">
                             <Row>
                                 <Col md="6" style={styles.col}>
                                     <Zoom>
-                                         <ImagePhotoFull src={Image5} />
+                                        <ImagePhotoFull src={Image5} />
                                     </Zoom>
                                 </Col>
                                 <Col md="6" style={styles.col}>
                                     <Zoom>
-                                         <ImagePhotoFull src={Image1} />
-                                    </Zoom>         
+                                        <ImagePhotoFull src={Image1} />
+                                    </Zoom>
                                 </Col>
                             </Row>
                             <Row>
-                                 <Zoom>
-                                     <ImagePhotoFull src={Image7} />
-                                </Zoom>                      
+                                <Zoom>
+                                    <ImagePhotoFull src={Image7} />
+                                </Zoom>
                             </Row>
                         </Col>
                         <Col md="5">
                             <Row>
                                 <Zoom>
-                                     <ImagePhotoFull src={Image4} />
-                                </Zoom>    
+                                    <ImagePhotoFull src={Image4} />
+                                </Zoom>
                             </Row>
                             <Row>
                                 <Zoom>
-                                     <ImagePhotoFull src={Image6} />
-                                </Zoom>                        
+                                    <ImagePhotoFull src={Image6} />
+                                </Zoom>
                             </Row>
                         </Col>
-                        </Row>
-                        <Row>
-                            <Col md="4" style={styles.col}>
-                                <Zoom>
-                                     <ImagePhotoFull src={Image8} />
-                                </Zoom>    
-                            </Col>
-                            <Col md="4" style={styles.col}>
-                                <Zoom>
-                                     <ImagePhotoFull src={Image2} />
-                                </Zoom>                               
-                            </Col>
-                            <Col md="4" style={styles.col}>
-                                <Zoom>
-                                     <ImagePhotoFull src={Image3} />
-                                </Zoom>                           
-                            </Col>
-                        </Row>
-                    </PhotoContainer>
+                    </Row>
+                    <Row>
+                        <Col md="4" style={styles.col}>
+                            <Zoom>
+                                <ImagePhotoFull src={Image8} />
+                            </Zoom>
+                        </Col>
+                        <Col md="4" style={styles.col}>
+                            <Zoom>
+                                <ImagePhotoFull src={Image2} />
+                            </Zoom>
+                        </Col>
+                        <Col md="4" style={styles.col}>
+                            <Zoom>
+                                <ImagePhotoFull src={Image3} />
+                            </Zoom>
+                        </Col>
+                    </Row>
+                </PhotoContainer>
             </Container>
         </>
     );
