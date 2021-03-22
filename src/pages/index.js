b16235fbe647c6ed410ddd72a151875f84148e4c
KrukowskiJ <jakub.krukowski@student.wat.edu.pl>
2021-03-22 02:45:14 +0100

    add page loader ?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components"
-import * as React from "react"
+import React, { Component } from "react";
 import NavBar from "../components/NavBar"
 import Team from "../components/Team"
 import OurExpertise from "../components/OurExpertise"
@@ -10,19 +10,46 @@ import "slick-carousel/slick/slick-theme.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Gallery from "../components/Gallery"
 import Stopka from "../components/Stopka"
+import Loader from "react-spinners/PacmanLoader";
+import { css } from "@emotion/core";
 // markup
-const IndexPage = () => {
-  return (
-    <>
-      <NavBar />
-      <OurExpertise />
-      <Team />
-      <AboutUS />
-      <Gallery />
-      <Stopka />
-    </>
-  )
+
+class IndexPage extends Component {
+  state = { loading: true };
+  sleep = milliseconds => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+  };
+  wait = async (milliseconds = 2000) => {
+    await this.sleep(milliseconds);
+    this.setState({
+      loading: false
+    });
+  };
+  componentDidMount() {
+    this.wait(2000);
+  }
+
+  render() {
+    if (this.state.loading) return <Loader color={"red"} css={override} size={100} />;
+
+    return (
+      <>
+        <NavBar />
+        <OurExpertise />
+        <Team />
+        <AboutUS />
+        <Gallery />
+        <Stopka />
+      </>
+    );
+  }
 }
+
+const override = css`
+position: absolute;
+top:40%;
+left: 40%;
+`;
 
 export default IndexPage
 
